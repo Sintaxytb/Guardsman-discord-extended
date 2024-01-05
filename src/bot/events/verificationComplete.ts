37a6d5f8b75ceb13a44bb50d5fcd5a3ef87df13a
imskyyc <sky@imskyyc.com>
2024-01-04 22:41:54 -0800

    add try/catch block to verificationCallback

--- src/bot/events/verificationComplete.ts
+++ src/bot/events/verificationComplete.ts
@@ -5,22 +5,29 @@ export default async (guardsman: Guardsman, discordId: string) =>
 {
     const interaction = guardsman.bot.pendingVerificationInteractions[discordId];
 
-    const reply = await interaction.editReply({
-        components: [],
-
-        embeds: [
-        new EmbedBuilder()
-            .setTitle(`Guardsman Verification`)
-            .setDescription(
-                "Discord account verification was successful! Please run `/update` to obtain roles."
-            )
-            .setColor(Colors.Green)
-            .setTimestamp()
-            .setFooter({
-                text: "Guardsman Verification"
-            }),
-        ],
-    });
-
-    await reply.reply(`<@${interaction.member.id}>`)
+    try 
+    {
+        const reply = await interaction.editReply({
+            components: [],
+    
+            embeds: [
+            new EmbedBuilder()
+                .setTitle(`Guardsman Verification`)
+                .setDescription(
+                    "Discord account verification was successful! Please run `/update` to obtain roles."
+                )
+                .setColor(Colors.Green)
+                .setTimestamp()
+                .setFooter({
+                    text: "Guardsman Verification"
+                }),
+            ],
+        });
+    
+        await reply.reply(`<@${interaction.member.id}>`)
+    } 
+    catch (error) 
+    {
+        guardsman.log.warn(`Failed to respond to interaction ${interaction.id}`)
+    }
 }
