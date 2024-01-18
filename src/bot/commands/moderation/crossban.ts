8e166301764a3dfe5ff58900d2dcd945459283ab
imskyyc <sky@imskyyc.com>
2024-01-17 17:22:52 -0800

    add crossban and finduser commands

---
+++ src/bot/commands/moderation/crossban.ts
@@ -0,0 +1,154 @@
+import { AxiosResponse } from "axios";
+import { ChatInputCommandInteraction, Colors, EmbedBuilder, SlashCommandIntegerOption, SlashCommandStringOption } from "discord.js";
+import { Guardsman } from "index";
+
+export default class CrossBanCommand implements ICommand 
+{
+    name: Lowercase<string> = "cban";
+    description: string = "Allows Guardsman moderators to cross ban a user from ALL Guardsman-controlled servers.";
+    guardsman: Guardsman;
+
+    options = [
+        new SlashCommandStringOption()
+            .setName("id")
+            .setDescription("The Discord ID of the user to ban")
+            .setRequired(true),
+
+        new SlashCommandStringOption()
+            .setName("reason")
+            .setDescription("The reason for the ban")
+            .setRequired(false)
+    ]
+
+    constructor(guardsman: Guardsman) 
+    {
+        this.guardsman = guardsman;
+    }
+
+    async execute(interaction: ChatInputCommandInteraction<"cached">): Promise<void>
+    {
+        await interaction.deferReply();
+
+        const discordId = interaction.options.getString("id", true);
+        const banReason = interaction.options.getString("reason", false);
+
+        const canGlobalBan = await this.guardsman.userbase.checkPermissionNode(interaction.user, "moderate:moderate");
+
+        if (!canGlobalBan) 
+        {
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Guardsman API")
+                        .setDescription("You do not have permission to `moderate:moderate`")
+                        .setColor(Colors.Red)
+                        .setFooter({ text: "Guardsman API" })
+                        .setTimestamp()
+                ]
+            });
+
+            return;
+        };
+
+        let userData: any;
+
+        try 
+        {
+            userData = await this.guardsman.backend.get(`discord/user/by-discord/${discordId}`);
+        } 
+        catch (error) 
+        {}
+
+        const executingPosition = await this.guardsman.userbase.getPermissionLevel(interaction.member.user);
+        if (userData != undefined && userData.data.position >= executingPosition) 
+        {
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Guardsman Moderation")
+                        .setDescription(`Your permission level is not high enough to global ban ${userData.data.username}. (${executingPosition}=>${userData.data.position})`)
+                        .setColor(Colors.Red)
+                        .setFooter({ text: "Guardsman API" })
+                        .setTimestamp()
+                ]
+            })
+            
+            return;
+        }
+
+        // send ban dm to user
+        try 
+        {
+            const user = await this.guardsman.bot.users.cache.find(user => user.id === discordId);
+            if (!user) throw new Error("User could not be messaged.");
+
+            await user.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Guardsman Moderation")
+                        .setDescription("You have been **globally banned** from ALL Guardsman-controlled guilds, and ALL Guardsman-controlled experiences.")
+                        .setColor(Colors.Red)
+                        .setFooter({ text: "Guardsman Moderation"})
+                        .setTimestamp()
+                        .addFields(
+                            {
+                                name: "Reason",
+                                value: banReason || "No Reason Provided"
+                            }
+                        )
+                ]
+            })
+        } 
+        catch (error) 
+        {
+            await interaction.channel?.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Guardsman Moderation")
+                        .setDescription(`Failed to send ban DM. ${error}`)
+                        .setColor(Colors.Orange)
+                        .setFooter({ text: "Guardsman API" })
+                        .setTimestamp()
+                ]
+            });
+        }
+
+        const guilds = this.guardsman.bot.guilds.cache.values();
+        const errors = [];
+        for (const guild of guilds) 
+        {
+            try 
+            {
+                await guild.bans.create(userData.data.discord_id, {
+                    reason: (banReason || `No reason provided.`) + `; Executed by: ${interaction.member.user.username}`
+                });
+            } 
+            catch (error) 
+            {
+                errors.push(error);
+            }
+        }
+
+        await interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                        .setTitle("Guardsman Moderation")
+                        .setDescription(`${userData.data.username} has been globally banned from all Guardsman-controlled guilds and experiences.`)
+                        .setColor(Colors.Red)
+                        .setFooter({ text: "Guardsman Moderation"})
+                        .setTimestamp()
+                        .addFields(
+                            {
+                                name: "Reason",
+                                value: banReason || "No Reason Provided"
+                            },
+
+                            {
+                                name: "Errors",
+                                value: errors.length > 0 && errors.join(",\n") || "None."
+                            }
+                        )
+            ]
+        })
+    }
+}
---
+++ src/bot/commands/moderation/finduser.ts
@@ -0,0 +1,112 @@
+import { AutocompleteInteraction, ChatInputCommandInteraction, Colors, EmbedBuilder, SlashCommandStringOption } from "discord.js";
+import { Guardsman } from "index";
+import axios, { AxiosResponse } from "axios";
+
+interface IRobloxUser {
+    previousUsernames: string[],
+    hasVerifiedBadge: boolean,
+    id: number,
+    name: string,
+    displayName: string
+}
+
+interface IUserSearchResponse {
+    data: IRobloxUser[]
+}
+
+export default class FindUserCommand implements ICommand 
+{
+    name: Lowercase<string> = "finduser";
+    description: string = "Allows guild moderators to search for a user's ROBLOX profile";
+    guardsman: Guardsman;
+
+    options = [
+        new SlashCommandStringOption()
+            .setName("username")
+            .setDescription("ROBLOX username")
+            .setRequired(true)
+            .setAutocomplete(true)
+    ]
+
+    constructor(guardsman: Guardsman) 
+    {
+        this.guardsman = guardsman;
+    }
+
+    async execute(interaction: ChatInputCommandInteraction<"cached">): Promise<void>
+    {
+        await interaction.deferReply();
+
+        const query = interaction.options.getString("username", true);
+
+        await axios.post<IUserSearchResponse>(`https://users.roblox.com/v1/usernames/users/`, {
+            usernames: [query],
+            excludeBannedUsers: false
+        }, 
+        {
+            headers: {
+                Cookie: `.ROBLOSECURITY=${this.guardsman.environment.ROBLOX_COOKIE}`
+            }
+        })
+            .then(async response => {
+                const user = response.data.data[0];
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(user.name)
+                            .setColor(Colors.Green)
+                            .setFooter({ text: "Guardsman Database" })
+                            .setTimestamp()
+                            .addFields(
+                                {
+                                    name: "Username",
+                                    value: user.name,
+                                    inline: true
+                                },
+
+                                {
+                                    name: "ROBLOX ID",
+                                    value: user.id.toString(),
+                                    inline: true
+                                },
+
+                                {
+                                    name: "Profile Link",
+                                    value: `https://www.roblox.com/users/${user.id}/profile`,
+                                    inline: true
+                                }
+                            )
+                    ]
+                })
+            })
+            .catch(async response => {
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle("Guardsman Moderation")
+                            .setDescription(`An error occurred whilst fetching the user. ${response}`)
+                            .setColor(Colors.Red)
+                            .setFooter({ text: "ROBLOX API" })
+                            .setTimestamp()
+                    ]
+                })
+            })
+    }
+
+    async autocomplete(interaction: AutocompleteInteraction<"cached">): Promise<void> {
+        const query = interaction.options.getString("username", true);
+        if (query == "") return;
+
+        await axios.get<IUserSearchResponse>(`https://users.roblox.com/v1/users/search?keyword=${query}&limit=10`, {
+            headers: {
+                Cookie: `.ROBLOSECURITY=${this.guardsman.environment.ROBLOX_COOKIE}`
+            }
+        })
+            .then(response => {
+                interaction.respond(response.data.data.map(user => { return { name: user.name, value: user.name } }));
+            })
+            .catch(response => {
+                interaction.respond([])
+            })
+    }
+}
