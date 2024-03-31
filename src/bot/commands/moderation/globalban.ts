2495b76424a8b5873e0839a8a2df555e4ea53e00
bigbenster702 <52-bigbenster702@users.noreply.git.bunkerbravointeractive.com>
2024-03-31 17:08:10 -0500

    bleh x2

--- src/bot/commands/moderation/globalban.ts
+++ src/bot/commands/moderation/globalban.ts
@@ -96,34 +96,36 @@ export default class GlobalBanCommand implements ICommand
             return;
         }
 
-        if (banExpiry && banExpiry < Date.now()) {
-            await interaction.editReply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle("Guardsman Moderation")
-                        .setDescription("You cannot ban a user in the past.")
-                        .setColor(Colors.Red)
-                        .setFooter({ text: "Guardsman Moderation" })
-                        .setTimestamp()
-                ]
-            });
+        if (banExpiry) {
+            if (banExpiry < Date.now()) {
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle("Guardsman Moderation")
+                            .setDescription("You cannot ban a user in the past.")
+                            .setColor(Colors.Red)
+                            .setFooter({ text: "Guardsman Moderation" })
+                            .setTimestamp()
+                    ]
+                });
 
-            return;
-        }
+                return;
+            }
 
-        if (banExpiry && !isNaN(Date.parse(new Date(banExpiry).toString()))) {
-            await interaction.editReply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle("Guardsman Moderation")
-                        .setDescription("Invalid expiry date.")
-                        .setColor(Colors.Red)
-                        .setFooter({ text: "Guardsman Moderation" })
-                        .setTimestamp()
-                ]
-            });
+            if (!isNaN(Date.parse(new Date(banExpiry).toString()))) {
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle("Guardsman Moderation")
+                            .setDescription("Invalid expiry date.")
+                            .setColor(Colors.Red)
+                            .setFooter({ text: "Guardsman Moderation" })
+                            .setTimestamp()
+                    ]
+                });
 
-            return;
+                return;
+            }
         }
 
         const banData: AxiosResponse<IAPIPunishmentData> = await this.guardsman.backend.post(`discord/user/${guardsmanId}/punishment`, {
