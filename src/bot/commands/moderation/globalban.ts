fa127337d21d1007e6d677475571b0802d196aef
bigbenster702 <52-bigbenster702@users.noreply.git.bunkerbravointeractive.com>
2024-03-31 20:15:20 -0500

    scrap that

--- src/bot/commands/moderation/globalban.ts
+++ src/bot/commands/moderation/globalban.ts
@@ -96,36 +96,19 @@ export default class GlobalBanCommand implements ICommand
             return;
         }
 
-        if (banExpiry) {
-            if (banExpiry < Math.floor(Date.now() / 1000)) {
-                await interaction.editReply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setTitle("Guardsman Moderation")
-                            .setDescription("You cannot ban a user in the past.")
-                            .setColor(Colors.Red)
-                            .setFooter({ text: "Guardsman Moderation" })
-                            .setTimestamp()
-                    ]
-                });
+        if (banExpiry && banExpiry < Math.floor(Date.now() / 1000)) {
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("Guardsman Moderation")
+                        .setDescription("You cannot ban a user in the past.")
+                        .setColor(Colors.Red)
+                        .setFooter({ text: "Guardsman Moderation" })
+                        .setTimestamp()
+                ]
+            });
 
-                return;
-            }
-
-            if (!isNaN(Date.parse(new Date(banExpiry).toString()))) {
-                await interaction.editReply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setTitle("Guardsman Moderation")
-                            .setDescription("Invalid expiry date.")
-                            .setColor(Colors.Red)
-                            .setFooter({ text: "Guardsman Moderation" })
-                            .setTimestamp()
-                    ]
-                });
-
-                return;
-            }
+            return;
         }
 
         const banData: AxiosResponse<IAPIPunishmentData> = await this.guardsman.backend.post(`discord/user/${guardsmanId}/punishment`, {
